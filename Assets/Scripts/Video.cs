e8a817c7f6885c0a31f37c0c94fdb4b46753f7e6
Bas Paap <paap@kissthefrog.nl>
2022-06-01 18:20:47 +0200

    Handle video fade in and out.

--- Assets/Scripts/Video.cs
+++ Assets/Scripts/Video.cs
@@ -6,14 +6,19 @@ using UnityEngine.Video;
 [RequireComponent(typeof(VideoPlayer))] 
 public class Video : MonoBehaviour
 {
-    [SerializeField] private Material rippleMaterial;
     [SerializeField] private VideoClip defaultClip;
-
+    [SerializeField, Range(0, 5)] private float fadeInDuration;
+    [SerializeField, Range(0, 5)] private float fadeOutDuration;
+    
     private VideoPlayer videoPlayer;
+    private float? fadeInStartTime;
+    private float? fadeOutStartTime;
+    private double fadeOutStartTimeInClip;
 
     private void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.targetCameraAlpha = 0;
 
         Controls.VideoTriggered += Controls_VideoTriggered;
     }
@@ -33,7 +38,46 @@ public class Video : MonoBehaviour
             return;
         }
 
+        fadeOutStartTimeInClip = clip.length - fadeOutDuration;
         videoPlayer.clip = clip;
         videoPlayer.Play();
+        fadeInStartTime = Time.time;
+    }
+
+    private void Update()
+    {
+        // Handle fade in if necessary.
+        if (fadeInStartTime.HasValue)
+        {
+            var alphaValue = Mathf.InverseLerp(fadeInStartTime.Value, fadeInStartTime.Value + fadeInDuration, Time.time);
+            videoPlayer.targetCameraAlpha = alphaValue;
+            
+            if (alphaValue >= 1.0f)
+            {
+                fadeInStartTime = null;
+            }
+        }        
+
+        // If the video is visible and it is time for the fade out to start, 
+        // record the current time to start it.
+        if (videoPlayer.targetCameraAlpha > 0 && 
+            !fadeOutStartTime.HasValue && 
+            videoPlayer.time >= fadeOutStartTimeInClip)
+        {
+            fadeOutStartTime = Time.time;
+        }
+
+        // Handle the fade out if necessary.
+        if (fadeOutStartTime.HasValue)
+        {
+            var alphaValue = 1 - Mathf.InverseLerp(fadeOutStartTime.Value, fadeOutStartTime.Value + fadeOutDuration, Time.time);
+            videoPlayer.targetCameraAlpha = alphaValue;
+
+            if (alphaValue <= 0.0f)
+            {
+                fadeOutStartTime = null;
+                videoPlayer.time = 0;
+            }
+        }
     }
 }
